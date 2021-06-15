376fe175040c1a365f836d8549edf212838a243e
nlewis84 <github email address>
2021-06-15 15:28:48 -0500

    deploy api to heroku

--- apolloschurchapp/ios/ChaseOaksTests/ChaseOaksTests.m
+++ apolloschurchapp/ios/ChaseOaksTests/ChaseOaksTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface apolloschurchappTests : XCTestCase
+@interface ChaseOaksTests : XCTestCase
 
 @end
 
-@implementation apolloschurchappTests
+@implementation ChaseOaksTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
