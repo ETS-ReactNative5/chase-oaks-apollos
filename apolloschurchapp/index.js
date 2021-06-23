8726855c2b2ff5acfb2801c7aeabacd16278c7fb
nlewis84 <github email address>
2021-06-23 09:39:18 -0500

    ready for iOS deployment

--- apolloschurchapp/index.js
+++ apolloschurchapp/index.js
@@ -1,15 +1,15 @@
-import './loadConfig';
-import { AppRegistry, YellowBox } from 'react-native';
-import ApollosConfig from '@apollosproject/config';
+import "./loadConfig";
+import { AppRegistry, YellowBox } from "react-native";
+import ApollosConfig from "@apollosproject/config";
 
 // temp fix for the promise.finally
 // https://github.com/storybookjs/storybook/issues/8371
 const fn = Promise.prototype.finally;
-const Storybook = require('./storybook').default; // eslint-disable-line
+const Storybook = require("./storybook").default; // eslint-disable-line
 Promise.prototype.finally = fn; // eslint-disable-line
-const useStorybook = ApollosConfig.STORYBOOK === 'true';
+const useStorybook = ApollosConfig.STORYBOOK === "true";
 
-const MainApp = require('./src').default;
+const MainApp = require("./src").default;
 
 let App = MainApp;
 if (useStorybook) {
@@ -17,8 +17,8 @@ if (useStorybook) {
 }
 
 YellowBox.ignoreWarnings([
-  'Warning: isMounted(...) is deprecated',
-  'Module RCTImageLoader',
+  "Warning: isMounted(...) is deprecated",
+  "Module RCTImageLoader",
 ]);
 
-AppRegistry.registerComponent('ChaseOaks', () => App);
+AppRegistry.registerComponent("ChaseOaks", () => App);
