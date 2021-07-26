1cdfefd53d845d2cd6e3695b47ab7c55e1499706
nlewis84 <nathan@differential.com>
2021-07-26 17:05:53 -0500

    updated provider.js to include theme

--- apolloschurchapp/src/Providers.js
+++ apolloschurchapp/src/Providers.js
@@ -1,7 +1,8 @@
+import querystring from 'querystring';
 import React from 'react';
-import PropTypes from 'prop-types';
+import { useColorScheme } from 'react-native';
 import ApollosConfig from '@apollosproject/config';
-import { NavigationService } from '@apollosproject/ui-kit';
+import { Providers, NavigationService } from '@apollosproject/ui-kit';
 import { AuthProvider } from '@apollosproject/ui-auth';
 import { AnalyticsProvider } from '@apollosproject/ui-analytics';
 import { NotificationsProvider } from '@apollosproject/ui-notifications';
@@ -10,55 +11,83 @@ import {
   ACCEPT_FOLLOW_REQUEST,
 } from '@apollosproject/ui-connected';
 import { checkOnboardingStatusAndNavigate } from '@apollosproject/ui-onboarding';
+import RNAmplitude from 'react-native-amplitude-analytics';
 
 import ClientProvider, { client } from './client';
+import customTheme, { customIcons } from './theme';
 
-const AppProviders = (props) => (
-  <ClientProvider {...props}>
-    <NotificationsProvider
-      oneSignalKey={ApollosConfig.ONE_SIGNAL_KEY}
-      // TODO deprecated prop
-      navigate={NavigationService.navigate}
-      handleExternalLink={(url) => {
-        const path = url.split('app-link/')[1];
-        const [route, location] = path.split('/');
-        if (route === 'content')
-          NavigationService.navigate('ContentSingle', { itemId: location });
-        if (route === 'nav')
-          NavigationService.navigate(
-            // turns "home" into "Home"
-            location[0].toUpperCase() + location.substring(1)
-          );
-      }}
-      actionMap={{
-        // accept a follow request when someone taps "accept" in a follow request push notification
-        acceptFollowRequest: ({ requestPersonId }) =>
-          client.mutate({
-            mutation: ACCEPT_FOLLOW_REQUEST,
-            variables: { personId: requestPersonId },
-          }),
-      }}
-    >
-      <AuthProvider
-        navigateToAuth={() => NavigationService.navigate('Auth')}
+const amplitude = new RNAmplitude(ApollosConfig.AMPLITUDE_API_KEY);
+
+const AppProviders = (props) => {
+  const islight = useColorScheme() === 'light';
+  return (
+    <ClientProvider {...props}>
+      <NotificationsProvider
+        oneSignalKey={ApollosConfig.ONE_SIGNAL_KEY}
+        // TODO deprecated prop
         navigate={NavigationService.navigate}
-        closeAuth={() =>
-          checkOnboardingStatusAndNavigate({
-            client,
-            navigation: NavigationService,
-          })
-        }
-      >
-        <AnalyticsProvider>
-          <LiveProvider>{props.children}</LiveProvider>
-        </AnalyticsProvider>
-      </AuthProvider>
-    </NotificationsProvider>
-  </ClientProvider>
-);
+        handleExternalLink={(url) => {
+          const path = url.includes('app-link/')
+            ? url.split('app-link/')[1]
+            : url.split('//')[1];
 
-AppProviders.propTypes = {
-  children: PropTypes.shape({}),
+          const [route, location] = path.split('/');
+          if (route === 'content')
+            NavigationService.navigate('ContentSingle', { itemId: location });
+          if (route === 'nav') {
+            const [cleanPath, query] = location.split('?');
+            const args = querystring.parse(query);
+            NavigationService.navigate(
+              // turns "home" into "Home"
+              cleanPath[0].toUpperCase() + cleanPath.substring(1),
+              args
+            );
+          }
+        }}
+        actionMap={{
+          // accept a follow request when someone taps "accept" in a follow request push notification
+          acceptFollowRequest: ({ requestPersonId }) =>
+            client.mutate({
+              mutation: ACCEPT_FOLLOW_REQUEST,
+              variables: { personId: requestPersonId },
+            }),
+        }}
+      >
+        <AuthProvider
+          navigateToAuth={() => NavigationService.navigate('Auth')}
+          navigate={NavigationService.navigate}
+          closeAuth={() =>
+            checkOnboardingStatusAndNavigate({
+              client,
+              navigation: NavigationService,
+            })
+          }
+        >
+          <AnalyticsProvider
+            trackFunctions={[
+              ({ eventName, properties }) =>
+                amplitude.logEvent(eventName, properties),
+            ]}
+          >
+            <LiveProvider>
+              <Providers
+                themeInput={{
+                  ...customTheme,
+                  ...{
+                    colors: islight
+                      ? customTheme.lightColors
+                      : customTheme.darkColors,
+                  },
+                }}
+                iconInput={customIcons}
+                {...props}
+              />
+            </LiveProvider>
+          </AnalyticsProvider>
+        </AuthProvider>
+      </NotificationsProvider>
+    </ClientProvider>
+  );
 };
 
 export default AppProviders;
