8743e801142770f8e2aae16b318d86b37308bcc2
Conrad VanLandingham <conrad@differential.com>
2021-09-03 12:47:37 -0500

    add basic getFeatures feed

--- apollos-church-api/src/data/ContentItem.js
+++ apollos-church-api/src/data/ContentItem.js
@@ -5,6 +5,20 @@ const { resolver, schema } = ContentItem;
 class dataSource extends ContentItem.dataSource {
   attributeIsVideo = ({ key }) =>
     key.toLowerCase().includes('video') || key.toLowerCase().includes('vimeo');
+
+  getFeatures = async (item) => {
+    const features = await super.getFeatures(item);
+    const { Feature } = this.context.dataSources;
+    // TODO: this pushes this feature to ALL Content Items
+    features.unshift(
+      Feature.createHorizontalCardListFeature({
+        title: "Your Next Steps",
+        // TODO, just grabs top 5 from one of their existing channels
+        algorithms: [{ type: 'CONTENT_FEED', arguments: { limit: 5, channelIds: [364] } }],
+      })
+    );
+    return features;
+  }
 }
 
 export { resolver, schema, dataSource };
