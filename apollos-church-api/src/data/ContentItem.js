2ab9bb9e1328dcaa480fb9d621d62f4f19c5a358
Conrad VanLandingham <conrad@differential.com>
2021-09-08 15:32:02 -0500

    fix expired content

--- apollos-church-api/src/data/ContentItem.js
+++ apollos-church-api/src/data/ContentItem.js
@@ -1,8 +1,23 @@
 import { ContentItem } from '@apollosproject/data-connector-rock';
+import moment from 'moment';
+import ApollosConfig from '@apollosproject/config';
+
+const { ROCK } = ApollosConfig;
 
 const { resolver, schema } = ContentItem;
 
 class dataSource extends ContentItem.dataSource {
+  LIVE_CONTENT = () => {
+    const date = moment()
+      .tz(ROCK.TIMEZONE)
+      .format()
+      .split(/[-+]\d+:\d+/)[0];
+    // the only difference between this and core is that this only looks
+    // at startdatetime. Sermon Messages have fake expire dates, so we ignore those.
+    const filter = `((StartDateTime lt datetime'${date}') or (StartDateTime eq null)) and ((Status eq 'Approved') or (ContentChannel/RequiresApproval eq false))`;
+    return filter;
+  };
+
   attributeIsVideo = ({ key }) =>
     key.toLowerCase().includes('video') || key.toLowerCase().includes('vimeo');
 
